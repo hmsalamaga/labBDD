38ed2c5b8eca8e66c93489b5f624a3477eae30dc
bakpawel <bakpawel103@gmail.com>
2020-06-18 16:03:28 +0200

    feature1_impl Implemented findNextDepartures method inside ItineraryServiceImplementation

--- src/main/java/edu/iis/mto/bdd/trains/services/ItineraryServiceImplementation.java
+++ src/main/java/edu/iis/mto/bdd/trains/services/ItineraryServiceImplementation.java
@@ -1,10 +1,14 @@
 package edu.iis.mto.bdd.trains.services;
 
+import edu.iis.mto.bdd.trains.model.Line;
 import org.joda.time.LocalTime;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItineraryServiceImplementation implements ItineraryService {
     private TimetableService timetableService;
+    private final int MINUTES_TO_NEXT_DEPARTURE = 15;
 
     public ItineraryServiceImplementation(TimetableService timetableService) {
         this.timetableService = timetableService;
@@ -12,6 +16,20 @@ public class ItineraryServiceImplementation implements ItineraryService {
 
     @Override
     public List<LocalTime> findNextDepartures(String departure, String destination, LocalTime time) {
-        return null;
+        List<Line> lines = timetableService.findLinesThrough(departure, destination);
+        List<LocalTime> nextDepartures = new ArrayList<>();
+
+        List<LocalTime> tempDepartures;
+        for (Line line : lines) {
+            tempDepartures = timetableService.findArrivalTimes(line, departure);
+
+            for (LocalTime localTime : tempDepartures) {
+                if (localTime.isAfter(time) && localTime.minusMinutes(MINUTES_TO_NEXT_DEPARTURE).isBefore(time)) {
+                    nextDepartures.add(localTime);
+                }
+            }
+        }
+
+        return nextDepartures;
     }
 }
