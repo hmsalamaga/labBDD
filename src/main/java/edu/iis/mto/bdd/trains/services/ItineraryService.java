e454188413b7ed6cc4165574fc806d9bdd24ace9
Lis-Marciniak.Jędrzej <215799@edu.p.lodz.pl>
2020-06-24 02:42:08 +0200

    [feature1_change] Max time of arrival offset changed to 30 min, extended "znajdź_odjazdy_następnych_pociągów" scenario

--- src/main/java/edu/iis/mto/bdd/trains/services/ItineraryService.java
+++ src/main/java/edu/iis/mto/bdd/trains/services/ItineraryService.java
@@ -5,7 +5,7 @@ import org.joda.time.LocalTime;
 import java.util.List;
 
 public interface ItineraryService {
-    int MAX_TRAVEL_TIME = 15;
+    int MAX_TRAVEL_TIME = 30;
 
     List<LocalTime> findNextDepartures(String origin, String destination, LocalTime time);
 }
