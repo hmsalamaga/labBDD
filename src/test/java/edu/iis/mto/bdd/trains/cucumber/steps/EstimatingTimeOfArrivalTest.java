998afd701abccefae0481a979c3d59d7d64ca047
Kowalczyk.Bartosz <215772@edu.p.lodz.pl>
2020-07-01 10:37:26 +0200

    fature2_rev1 added examples to scenario and made scenario an outline
    Signed-off-by: Kowalczyk.Bartosz <215772@edu.p.lodz.pl>

--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/EstimatingTimeOfArrivalTest.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/EstimatingTimeOfArrivalTest.java
@@ -1,10 +1,13 @@
 package edu.iis.mto.bdd.trains.cucumber.steps;
 
 import cucumber.api.PendingException;
+import cucumber.api.Transform;
 import cucumber.api.java.pl.Gdy;
 import cucumber.api.java.pl.I;
 import cucumber.api.java.pl.Wtedy;
 import cucumber.api.java.pl.Zakładając;
+
+import java.time.LocalTime;
 
 public class EstimatingTimeOfArrivalTest {
     @Zakładając("^chcę się dostać z \"([^\"]*)\" do \"([^\"]*)\"$")
@@ -13,8 +16,10 @@ public class EstimatingTimeOfArrivalTest {
         throw new PendingException();
     }
 
-    @I("^następny pociąg odjeżdża o (\\d+):(\\d+) na linii \"([^\"]*)\"$")
-    public void następnyPociągOdjeżdżaONaLinii(int arg0, int arg1, String arg2) throws Throwable {
+    @I("^następny pociąg odjeżdża o \"([^\"]*)\" na linii \"([^\"]*)\"$")
+    public void następnyPociągOdjeżdżaONaLinii(
+            @Transform(JodaLocalTimeConverter.class) LocalTime arg0, String arg1)
+            throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
@@ -24,8 +29,11 @@ public class EstimatingTimeOfArrivalTest {
 
     }
 
-    @Wtedy("^powinienem uzyskać następujący szacowany czas przyjazdu: (\\d+):(\\d+)$")
-    public void powinienemUzyskaćNastępującySzacowanyCzasPrzyjazdu(int arg0, int arg1) {
+    @Wtedy("^powinienem uzyskać następujący szacowany czas przyjazdu: \"([^\"]*)\"$")
+    public void powinienemUzyskaćNastępującySzacowanyCzasPrzyjazdu(
+            @Transform(JodaLocalTimeConverter.class) LocalTime arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
     }
 }
 
