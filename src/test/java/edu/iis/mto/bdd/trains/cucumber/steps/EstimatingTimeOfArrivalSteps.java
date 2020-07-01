754d058d9162342bf5c2759cb03b46aa7c54ccdb
Kowalczyk.Bartosz <215772@edu.p.lodz.pl>
2020-07-01 11:35:22 +0200

    fature2_rev1 renamed file to better suit scenarios
    Signed-off-by: Kowalczyk.Bartosz <215772@edu.p.lodz.pl>

--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/EstimatingTimeOfArrivalSteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/EstimatingTimeOfArrivalSteps.java
@@ -9,7 +9,7 @@ import cucumber.api.java.pl.Zakładając;
 
 import java.time.LocalTime;
 
-public class EstimatingTimeOfArrivalTest {
+public class EstimatingTimeOfArrivalSteps {
     @Zakładając("^chcę się dostać z \"([^\"]*)\" do \"([^\"]*)\"$")
     public void chcęSięDostaćZDo(String arg0, String arg1) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
